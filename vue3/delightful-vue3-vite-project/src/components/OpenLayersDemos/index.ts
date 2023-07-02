941c293b7625d72297133f6037c620fb4695c033
jack <jackzhoumine@gmail.com>
2023-07-03 03:25:56 +0800

    chore: 更新用户代码片段

--- vue3/delightful-vue3-vite-project/src/components/OpenLayersDemos/index.ts
+++ vue3/delightful-vue3-vite-project/src/components/OpenLayersDemos/index.ts
@@ -2,8 +2,9 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-06-02 09:29:57
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-06-26 10:13:32
+ * @LastEditTime: 2023-07-02 16:42:46
  * @Description : 导出组件
  */
 export { default as OLInitMap } from './OLInitMap.vue'
 export { default as OLControl } from './OLControl.vue'
+export { default as PublicMapData } from './PublicMapData.vue'
