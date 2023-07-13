0a3b805f1f94ed8887ea4f85fef9dc14a9293703
ZhouQiJun <jackchou4job@163.com>
2023-07-13 20:33:53 +0800

    绘制图形

--- vue3/delightful-vue3-vite-project/src/components/OpenLayersDemos/DrawGeo.vue
+++ vue3/delightful-vue3-vite-project/src/components/OpenLayersDemos/DrawGeo.vue
@@ -2,72 +2,78 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-07-08 16:29:24
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-07-09 19:24:50
+ * @LastEditTime: 2023-07-13 20:33:01
  * @Description : 绘制几何图形
  * 参考 https://openlayers.org/en/latest/examples/draw-features.html
 -->
 <script lang="ts" setup>
-import { Map, View } from 'ol'
+import { Image, Map, View } from 'ol'
 
-import { Draw, Interaction } from 'ol/interaction'
+import { Draw, Interaction, Modify, Select, Snap } from 'ol/interaction'
 import { Tile, Vector as VectorLayer } from 'ol/layer'
 import { Vector as VectorSource, XYZ } from 'ol/source'
+import { Circle, Fill, Stroke, Style } from 'ol/style'
 
 import { GeoList } from './components'
+import { initMap } from './tool'
 
 const mapContainer = ref()
 
-onMounted(initMap)
-
 let map = null
 let vectorSource = null
-let draw = null
-function initMap() {
-  const tianDiTuKey = '4c409692826bccaca32ee3e1a74ba1b5'
-  // 矢量地图
-  const tianDiTuUrl2 = `http://t0.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${tianDiTuKey}`
-  const tianDiTuSource2 = new XYZ({
-    url: tianDiTuUrl2,
-  })
-  // 矢量注记
-  const tianDiTuUrl3 = `http://t0.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${tianDiTuKey}`
-  const tianDiTuSource3 = new XYZ({
-    url: tianDiTuUrl3,
-  })
+let vectorLayer = null
 
-  vectorSource = new VectorSource({
-    wrapX: false,
-  })
-  const vectorLayer = new VectorLayer({
-    source: vectorSource,
-  })
-  map = new Map({
-    target: mapContainer.value,
-    layers: [
-      new Tile({
-        source: tianDiTuSource2,
-      }),
-      new Tile({
-        source: tianDiTuSource3,
-      }),
-      // NOTE  不添加矢量图层，绘制的图形会被覆盖，绘制不了
-      vectorLayer,
-    ],
-    view: new View({
-      center: [106.675271, 26.579508],
-      zoom: 10,
-      projection: 'EPSG:4326',
-    }),
-  })
+let draw = null
+let modify = null
+let select = null
+let snap = null
+
+onMounted(() => {
+  const result = initMap({ target: mapContainer.value })
+  map = result.map
+  // TODO 添加矢量图层
+  const obj = createVectorLayer()
+  vectorSource = obj.vectorSource
+  vectorLayer = obj.vectorLayer
+  map.addLayer(vectorLayer)
+
+  // // 选中
+  // select = new Select({
+  //   layers: [vectorLayer],
+  //   // style
+  // })
+  // map.addInteraction(select)
+
+  // // 修改
+  // modify = new Modify({
+  //   // source: vectorSource,
+  //   // 使用选中的要素
+  //   features: select.getFeatures(),
+  // })
+  // map.addInteraction(modify)
+})
+
+function drewFeatures(type = 'Point') {
+  addSnap()
+  addDraw(type)
 }
 
-// TODO 如何导入 Draw 参数类型
-function addInteraction(type: any) {
+function addDraw(type) {
+  // TODO 添加的顺序有要求吗
+  draw && map.removeInteraction(draw)
   draw = new Draw({
     source: vectorSource,
-    type,
+    type: type, // 'Point', 'LineString', 'Polygon', 'Circle'
   })
   map.addInteraction(draw)
+}
+function addSnap() {
+  // 捕捉
+  snap && map.removeInteraction(snap)
+  snap = new Snap({
+    source: vectorSource,
+  })
+  map.addInteraction(snap)
 }
 
 function undo() {
@@ -76,11 +82,43 @@ function undo() {
   draw.removeLastPoint()
   // vectorSource.clear()
 }
+
+function createVectorLayer() {
+  const vectorSource = new VectorSource({
+    wrapX: false,
+    features: [],
+  })
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+    style: features => {
+      return new Style({
+        fill: new Fill({
+          color: 'rgba(255, 0,0, 0.2)',
+        }),
+        stroke: new Stroke({
+          color: '#f00',
+          width: 2,
+        }),
+        image: new Circle({
+          radius: 10,
+          fill: new Fill({
+            color: 'rgba(255, 0,0, 0.8)',
+          }),
+          stroke: new Stroke({
+            color: '#fff',
+            width: 2,
+          }),
+        }),
+      })
+    },
+  })
+  return { vectorSource, vectorLayer }
+}
 </script>
 
 <template>
   <div class="draw-geo" ref="mapContainer">
-    <GeoList @select="addInteraction" @undo="undo" />
+    <GeoList @select="drewFeatures" @undo="undo" />
   </div>
 </template>
 
