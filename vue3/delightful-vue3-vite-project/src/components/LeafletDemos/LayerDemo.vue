3a642a58d2aac43fa2da3acddd3b9a643145f14b
jack <jackzhoumine@gmail.com>
2023-07-16 21:09:54 +0800

    添加wms图层

--- vue3/delightful-vue3-vite-project/src/components/LeafletDemos/LayerDemo.vue
+++ vue3/delightful-vue3-vite-project/src/components/LeafletDemos/LayerDemo.vue
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-04-08 23:03:17
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-07-16 20:46:33
+ * @LastEditTime: 2023-07-16 21:08:46
  * @Description : 图层类型
  * 瓦片图层
 -->
@@ -13,11 +13,15 @@ const tianDiTuKey = '4c409692826bccaca32ee3e1a74ba1b5'
 
 let map = null
 let layer = null
-
+let wmsLayer = null
+let staticImgLayer = null
 // 矢量注记
 const tianDiTuUrl3 = `http://t0.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${tianDiTuKey}`
 // 影像注记
 const tianDiTuUrl5 = `http://t0.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=${tianDiTuKey}`
+
+// lzugis WMS 图层
+const lzugisWMS = 'https://lzugis.cn/geoserver/lzugis/wms'
 
 const mapContainer = ref()
 
@@ -29,7 +33,7 @@ watch(
   () => currentLayer.url,
   val => {
     // TODO setUrl 用于切换 url
-    layer.setUrl(val)
+    // layer.setUrl(val)
   }
 )
 
@@ -51,13 +55,20 @@ const layers = [
 onMounted(() => {
   map = initMap(mapContainer.value)
   layer = createLayer(currentLayer.url)
-  layer.addTo(map)
+  wmsLayer = createWMSLayer('https://lzugis.cn/geoserver/lzugis/wms', 'lzugis:city')
+  staticImgLayer = createStaticImgLayer(
+    './china.png',
+    [-12.575765437499996, 67.60788818750001, 65.7347814375, 145.7426538125]
+  )
+  wmsLayer.addTo(map)
+  const added = layer.addTo(map)
+  // staticImgLayer.addTo(map)
 })
 
 function initMap(
   mapContainer: HTMLElement,
   coordinates: [number, number] = [26.55, 106.6],
-  zoom: number = 13
+  zoom: number = 5
 ) {
   const map = L.map(mapContainer).setView(coordinates, zoom)
   return map
@@ -73,6 +84,26 @@ function createLayer(url) {
     maxZoom: 19,
   })
   return layer
+}
+/**
+ * wms 图层
+ * @param url
+ * @param layers
+ */
+function createWMSLayer(url, layers) {
+  return L.tileLayer.wms(url, {
+    layers,
+    format: 'image/png',
+    transparent: true,
+  })
+}
+/**
+ * 静态图片图层
+ * @param url
+ * @param extend
+ */
+function createStaticImgLayer(url, extend) {
+  return L.imageOverlay(url, extend)
 }
 </script>
 
