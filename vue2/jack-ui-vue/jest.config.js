4a5bc66a6c36bddf15ccdde96fbcf8971add9a6f
ZhouQiJun <jackchou4job@163.com>
2023-07-22 09:41:06 +0800

    chore: 修改测试代码的路径

--- vue2/jack-ui-vue/jest.config.js
+++ vue2/jack-ui-vue/jest.config.js
@@ -2,10 +2,10 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-07-20 21:51:03
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-07-22 00:43:53
+ * @LastEditTime: 2023-07-22 09:40:44
  * @Description : jest 配置文件
  */
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  // testMatch: ['./**/*.spec.{j,t}s?(x)', './**/*.test.{j,t}s?(x)'],
+  testMatch: ['./**/*.spec.{j,t}s?(x)', './**/*.test.{j,t}s?(x)'],
 }
