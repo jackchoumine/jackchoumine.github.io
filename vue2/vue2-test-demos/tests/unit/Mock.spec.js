b0f623806b6eed0e2f966493b8b858cf040c74e2
jack <jackzhoumine@gmail.com>
2023-07-23 00:53:39 +0800

    chore: 安装依赖

--- vue2/vue2-test-demos/tests/unit/Mock.spec.js
+++ vue2/vue2-test-demos/tests/unit/Mock.spec.js
@@ -2,14 +2,16 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-07-22 17:57:30
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-07-22 18:31:49
+ * @LastEditTime: 2023-07-23 00:52:28
  * @Description : 测试原型上的方法
  */
 import { shallowMount } from '@vue/test-utils'
 
 const CallVuePrototypePropDemo = {
   template: '<div>{{count}}</div>',
-  data: () => ({}),
+  data: () => ({
+    count: 0,
+  }),
   methods: {},
   mounted() {
     this.$bar.start()
