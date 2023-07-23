4b8cc43c57e72236b117192101594483ee7c6721
jack <jackzhoumine@gmail.com>
2023-07-23 18:18:53 +0800

    chore: 配置eslint

--- vue2/vue2-test-demos/.eslintrc.js
+++ vue2/vue2-test-demos/.eslintrc.js
@@ -1,29 +1,34 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2023-07-23 03:23:18
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2023-07-23 18:16:42
+ * @Description : eslint配置
+ */
 module.exports = {
-  root: true,
+  root: false,
+  // root: './',
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: [1, 'single'], //单引号
+    'prettier/prettier': 'off',
+    'no-unused-vars': 0,
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
     },
   ],
-};
+}
