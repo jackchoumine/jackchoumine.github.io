848ecfd6c38f9a3812ac9c3532572a3d52b5c7e8
jack <jackzhoumine@gmail.com>
2023-07-23 21:01:15 +0800

    测试异步代码

--- vue3/vue3-jest-test/tests/unit/throw-error.spec.js
+++ vue3/vue3-jest-test/tests/unit/throw-error.spec.js
@@ -2,8 +2,8 @@
  * @Description : 抛出方法匹配
  * @Date        : 2022-06-07 07:02:36 +0800
  * @Author      : JackChou
- * @LastEditTime: 2023-01-12 01:04:35 +0800
- * @LastEditors : JackChou
+ * @LastEditTime: 2023-07-23 20:58:57
+ * @LastEditors : ZhouQiJun
  */
 // import { fetchData, githubUsers } from '../../src/async.fn.js'
 
@@ -11,34 +11,87 @@ const testFn = () => {
   throw new Error('test')
 }
 
+function asyncFn(callback) {
+  setTimeout(() => {
+    callback(30)
+  }, 1000)
+}
 describe('函数', () => {
-  test('should throw error', () => {
-    expect(testFn).toThrow()
-    expect(testFn).not.toThrow('a')
-    expect(testFn).toThrow('test')
-  })
-
-  // test('异步函数', done => {
-  //   fetchData(n => {
-  //     expect(n).toEqual(30)
-  //   })
-  //   done()
+  // test('should throw error', () => {
+  //   expect(testFn).toThrow()
+  //   expect(testFn).not.toThrow('a')
+  //   expect(testFn).toThrow('test')
   // })
-  test('函数返回 promise', () => {
-    // NOTE
-    //expect.assertions(1) // 必须执行一次 expect
-    // jest.setTimeout(1)// 设置过期时间
-    // return githubUsers()
-    //   .then(res => {
-    //     expect(res).toEqual(23)
-    //   })
-    //   .catch(error => {
-    //     console.log(error)
-    //   })
-  })
 
-  test('函数返回 promise', async () => {
-    // const res = await githubUsers()
-    expect(23).toEqual(23)
+  // it('async callback function', done => {
+  //   expect.assertions(1)
+  //   let finish = false
+  //   // jest.useFakeTimers()
+  //   setTimeout(() => {
+  //     finish = true
+  //     console.log('finish setTimeout callback', finish)
+  //     expect(finish).toBe(true)
+  //     done()
+  //   }, 1000)
+  //   // jest.advanceTimersByTime(1000)
+  //   console.log('finish', finish)
+  // })
+  // it('async promise function', done => {
+  //   expect.assertions(1)
+  //   jest.useFakeTimers()
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       console.log('finish setTimeout callback')
+  //       resolve(100)
+  //     }, 1000)
+  //   })
+  //   jest.advanceTimersByTime(1000)
+  //   promise.then(res => {
+  //     console.log('finish promise', res)
+  //     expect(res).toBe(100)
+  //     done()
+  //   })
+  //   console.log('finish')
+  // })
+  it('async promise function', async () => {
+    expect.assertions(1)
+    const promiseFn = () => {
+      return new Promise((resolve, reject) => {
+        resolve(100)
+      })
+    }
+    const res = await promiseFn()
+    console.log('finish promise', res)
+    expect(res).toBe(100)
+    console.log('finish')
   })
+  // test('异步函数', done => {
+  //   expect.assertions(1)
+  //   // fetchData(n => {
+  //   //   expect(n).toEqual(30)
+  //   // })
+  //   asyncFn(n => {
+  //     console.log(n, 'zqj log')
+  //     expect(n).toEqual(30)
+  //     done()
+  //   })
+  //   console.log(1, 'zqj log')
+  // })
+  // test('函数返回 promise', () => {
+  //   // NOTE
+  //   //expect.assertions(1) // 必须执行一次 expect
+  //   // jest.setTimeout(1)// 设置过期时间
+  //   // return githubUsers()
+  //   //   .then(res => {
+  //   //     expect(res).toEqual(23)
+  //   //   })
+  //   //   .catch(error => {
+  //   //     console.log(error)
+  //   //   })
+  // })
+
+  // test('函数返回 promise', async () => {
+  //   // const res = await githubUsers()
+  //   expect(23).toEqual(23)
+  // })
 })
