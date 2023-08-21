0e0bae37305bed7f01b4517b73ffa94e6f129a10
ZhouQiJun <jackchou4job@163.com>
2023-08-21 10:36:57 +0800

    feat: 原生弹窗

--- react/react-jest-test/src/components/MyInput.test.tsx
+++ react/react-jest-test/src/components/MyInput.test.tsx
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-08-19 19:04:14
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-08-20 16:19:43
+ * @LastEditTime: 2023-08-21 10:36:27
  * @Description :
  */
 import { render } from '@testing-library/react'
@@ -13,14 +13,17 @@ import { MyInput } from './MyInput'
 describe('MyInput.tsx', () => {
   it('测试用户输入', async () => {
     const { getByText, getByPlaceholderText, user } = setup(<MyInput />)
+    const input = getByPlaceholderText('请输入名字') as HTMLInputElement
     const name = 'jack'
-    const input = getByPlaceholderText('请输入名字')
+
+    expect(input.value).toEqual('')
 
     await user.type(input, name)
     const span = getByText(name, {
       selector: 'span',
     })
 
+    expect(input.value).toEqual(name)
     expect(span).not.toBeNull()
   })
 })
