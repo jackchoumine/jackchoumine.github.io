b708ddd4eb3d256b653bbe7718774edc0bf4ba10
ZhouQiJun <jackchou4job@163.com>
2023-09-01 15:13:52 +0800

    feat: 把业务提取到 js 中

--- vue3/vue3-jest-test/src/components/Counter.vue
+++ vue3/vue3-jest-test/src/components/Counter.vue
@@ -2,12 +2,12 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-09-01 10:31:25
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-09-01 10:31:36
+ * @LastEditTime: 2023-09-01 10:42:29
  * @Description : 
 -->
 <template>
-  <button role="increment" @click="count += 1" />
-  <button role="submit" @click="$emit('submit', count)" />
+  <button role="increment" @click="increment" />
+  <button role="submit" @click="submit" />
 </template>
 
 <script>
@@ -15,6 +15,17 @@ export default {
     data() {
         return {
             count: 0
+        }
+    },
+    methods:{
+        // NOTE 触发自定义事件的方法命名
+        // 1. onCustomEvent 和 触发的2023年9月1日 15:12:48保持一致
+        // 2. handleCustomEvent
+        submit(){
+            this.$emit('submit',this.count)
+        },
+        increment(){
+            this.count ++
         }
     }
 }
