117181984515640580060f657023640e35fbf8c1
ZhouQiJun <jackchou4job@163.com>
2023-09-01 15:28:22 +0800

    使用组合式api

--- vue3/vue3-jest-test/src/components/Counter.vue
+++ vue3/vue3-jest-test/src/components/Counter.vue
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-09-01 10:31:25
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-09-01 15:18:21
+ * @LastEditTime: 2023-09-01 15:26:11
  * @Description : 
 -->
 <template>
@@ -11,32 +11,33 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 export function submitValidator(value) {
     if (typeof value !== 'number') {
         throw Error(`Count should be a number. Got: ${count}`)
     }
     return true
 }
-
 export default {
     emits: {
         submit: submitValidator
     },
-    data() {
-        return {
-            count: 0
-        }
-    },
-    methods: {
+    setup(props, ctx) {
+        const count = ref(0)
         // NOTE 触发自定义事件的方法命名
         // 1. onCustomEvent 和 触发的2023年9月1日 15:12:48保持一致
         // 2. handleCustomEvent
-        submit() {
-            this.$emit('submit', this.count)
-        },
-        increment() {
-            this.count++
+        function submit() {
+            ctx.emit('submit', this.count)
         }
-    }
+        function increment() {
+            count.value++
+        }
+        return {
+            count,
+            submit,
+            increment
+        }
+    },
 }
 </script>
