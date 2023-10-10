891659538582910bf6b15f8897f5d31c703c30f6
ZhouQiJun <jackchou4job@163.com>
2023-10-10 10:13:31 +0800

    feat: 如何在hook 中使用 emit

--- vue3/delightful-vue3-vite-project/src/hooks/useTestGlobalState.ts
+++ vue3/delightful-vue3-vite-project/src/hooks/useTestGlobalState.ts
@@ -5,10 +5,10 @@ const globalCount = ref(0)
 
 export function useTestGlobalState() {
   // NOTE 不能在 hook 中使用 emit
+  // const emit = defineEmits(['add-count'])
   // 两种解决办法：
   // 1. 使用 watch + emit 代替 【推荐】
   // 2. hook 参数接收 emit，在 hook 内部调用 emit 触发事件
-  // const emit = defineEmits(['add-count'])
   // 本地状态，会随着组件的销毁重置
   const localCount = ref(0)
 
