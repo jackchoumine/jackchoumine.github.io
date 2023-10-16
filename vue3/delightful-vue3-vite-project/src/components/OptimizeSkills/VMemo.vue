8903367a8f8156a2c4d1e6b369a12a817a2eb88e
ZhouQiJun <jackchou4job@163.com>
2023-10-16 09:04:25 +0800

    style: 精简注释

--- vue3/delightful-vue3-vite-project/src/components/OptimizeSkills/VMemo.vue
+++ vue3/delightful-vue3-vite-project/src/components/OptimizeSkills/VMemo.vue
@@ -1,16 +1,16 @@
 <!--
  * @Author      : ZhouQiJun
  * @Date        : 2023-01-05 09:57:27
- * @LastEditors : JackChou
- * @LastEditTime: 2023-01-06 23:17:00 +0800
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2023-10-16 09:03:19
  * @Description : v-memo 的使用
- 
- v-memo 用来缓存渲染结果，绑定的变量为一个数组，让数组变化时，重新渲染。
+
+ v-memo 用来缓存渲染结果，绑定的变量是一个数组，数组变化时，才重新渲染。
  <p v-memo="[el1,el2]"></p>
  空数组和v-once相同
  <p v-memo="[]"></p>
  <p v-once></p>
- NOTE v-memo 在 v-for 内部无效。v-memo 常常和 v-for 配合使用。
+ NOTE v-memo 常常和 v-for 配合使用，在 v-for 内部无效。
  v-memo 和 react 的 useMemo 返回 jsx 极为相似。
  <div v-memo="[a]"></div>
  const memoDiv = useMemo(()=><div></div>,[a])
