26c05172fdd4463e11083bc9ce40d2d6be3dd76a
ZhouQiJun <jackchou4job@163.com>
2023-10-17 14:36:09 +0800

    feat: v-if 触发异步组件的加载

---
+++ vue3/delightful-vue3-vite-project/src/components/AsyncComponent/LoadingComponent.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+// TODO
+</script>
+
+<template>
+  <div class="loading-component">
+    <p>async component is loading</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.loading-component {
+  color: gray;
+}
+</style>
---
+++ vue3/delightful-vue3-vite-project/src/views/AsyncComponent.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author      : ZhouQiJun
+ * @Date        : 2023-10-17 10:32:29
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2023-10-17 10:37:49
+ * @Description : 异步组件实战
+-->
+<script lang="ts" setup>
+import { DemoOne } from '@/components/AsyncComponent'
+</script>
+
+<template>
+  <div class="async-component">
+    <h3>异步组件</h3>
+    <DemoOne />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.async-component {
+  // scss code
+}
+</style>
