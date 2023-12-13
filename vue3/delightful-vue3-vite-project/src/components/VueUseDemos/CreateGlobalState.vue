096d0ab7e8ee497262a5f89fbad9e135b536b4a7
jack <jackzhoumine@gmail.com>
2023-12-13 12:44:39 +0800

    feat: 使用闭包创建全局状态

--- vue3/delightful-vue3-vite-project/src/components/VueUseDemos/CreateGlobalState.vue
+++ vue3/delightful-vue3-vite-project/src/components/VueUseDemos/CreateGlobalState.vue
@@ -2,18 +2,22 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-05-15 16:44:23
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-05-15 16:49:42
+ * @LastEditTime: 2023-12-13 12:14:21
  * @Description : 
 -->
 <script lang="ts" setup name="CreateGlobalState">
-import { useGlobalCounter } from './store'
+import { useGlobalCounter, useOpen, useOpen2 } from './store'
 
 const { count } = useGlobalCounter()
+const { isOpen } = useOpen()
+const { isOpen: isOpen2 } = useOpen2()
 </script>
 
 <template>
   <div class="create-global-state">
-    <p>count{{ count }}</p>
+    <p>count {{ count }}</p>
+    <p>isOpen {{ isOpen }}</p>
+    <p>isOpen2 {{ isOpen2 }}</p>
   </div>
 </template>
 
--- vue3/delightful-vue3-vite-project/src/components/VueUseDemos/CreateGlobalStateTwo.vue
+++ vue3/delightful-vue3-vite-project/src/components/VueUseDemos/CreateGlobalStateTwo.vue
@@ -2,13 +2,16 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-05-15 16:44:23
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-05-15 16:55:34
+ * @LastEditTime: 2023-12-13 12:12:52
  * @Description : 
 -->
 <script lang="ts" setup name="CreateGlobalState">
-import { useGlobalCounter } from './store'
+import { useGlobalCounter, useOpen, useOpen2 } from './store'
 
 const { count } = useGlobalCounter()
+const { isOpen, toggleOpen } = useOpen()
+const { isOpen: isOpen2, toggleOpen: toggleOpen2 } = useOpen2()
+
 function decrement() {
   count.value--
 }
@@ -20,14 +23,23 @@ function increment() {
 <template>
   <div class="create-global-state">
     <h4>修改全局状态</h4>
-    <button @click="decrement">-</button>
-    <button @click="increment">+</button>
-    <p>count{{ count }}</p>
+    <button @click="decrement" class="btn-min">-</button>
+    <button @click="increment" class="btn-plus">+</button>
+    <p>count {{ count }}</p>
+    <h4>使用全局变量保存全局状态</h4>
+    <ElButton @click="() => toggleOpen()" type="primary">切换 open {{ isOpen }}</ElButton>
+    <ElButton @click="() => toggleOpen2()" type="primary">
+      切换 open2 {{ isOpen2 }}
+    </ElButton>
   </div>
 </template>
 
 <style scoped lang="scss">
 .create-global-state {
   // scss code
+  .btn-plus,
+  .btn-min {
+    width: 50px;
+  }
 }
 </style>
