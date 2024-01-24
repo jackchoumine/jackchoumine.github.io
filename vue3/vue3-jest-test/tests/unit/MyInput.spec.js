390fa44afce4ad5bd0e82ae88f1de27eff4a176f
ZhouQiJun <jackchou4job@163.com>
2024-01-24 16:08:11 +0800

    chore: 修改 jest 配置

--- vue3/vue3-jest-test/tests/unit/MyInput.spec.js
+++ vue3/vue3-jest-test/tests/unit/MyInput.spec.js
@@ -2,32 +2,32 @@
  * @Description : 测试 props
  * @Date        : 2023-01-12 00:12:23 +0800
  * @Author      : JackChou
- * @LastEditTime: 2023-01-12 00:24:01 +0800
- * @LastEditors : JackChou
+ * @LastEditTime: 2024-01-24 16:07:26
+ * @LastEditors : ZhouQiJun
  */
-import { shallowMount } from "@vue/test-utils";
-import MyInput from "./MyInput.vue";
+import { shallowMount } from '@vue/test-utils'
+import MyInput from './MyInput.vue'
 
-describe("测试 props", () => {
-  test("测试默认的 props", async () => {
-    const wrapper = shallowMount(MyInput);
+describe('测试 props', () => {
+  test('测试默认的 props', async () => {
+    const wrapper = shallowMount(MyInput)
     // NOTE 手动修改页面，vue的渲染是异步的
-    await wrapper.find("input").setValue("123");
-    expect(wrapper.find(".errror").exists()).toBe(false);
-  });
-  it("测试传递的 props", async () => {
+    await wrapper.find('input').setValue('123')
+    expect(wrapper.find('.errror').exists()).toBe(false)
+  })
+  it('测试传递的 props', async () => {
     const wrapper = shallowMount(MyInput, {
       props: {
         minLength: 5,
       },
-    });
-    await wrapper.find("input").setValue("12");
-    expect(wrapper.find(".error").exists()).toBe(true);
-  });
-  it("测试 setProps", async () => {
-    const wrapper = shallowMount(MyInput);
-    wrapper.setProps({ minLength: 5 });
-    await wrapper.find("input").setValue("12");
-    expect(wrapper.find(".error").exists()).toBe(true);
-  });
-});
+    })
+    await wrapper.find('input').setValue('12')
+    expect(wrapper.find('.error').exists()).toBe(true)
+  })
+  it('测试 setProps', async () => {
+    const wrapper = shallowMount(MyInput)
+    wrapper.setProps({ minLength: 5 })
+    await wrapper.find('input').setValue('1234')
+    expect(wrapper.find('.error').exists()).toBe(true)
+  })
+})
