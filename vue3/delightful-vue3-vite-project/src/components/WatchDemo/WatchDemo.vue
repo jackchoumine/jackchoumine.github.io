4d5b9af9fcd8488595b7c498a1b4183d34b4b911
ZhouQiJun <jackchou4job@163.com>
2024-01-26 12:07:22 +0800

    feat: 监听对象

--- vue3/delightful-vue3-vite-project/src/components/WatchDemo/WatchDemo.vue
+++ vue3/delightful-vue3-vite-project/src/components/WatchDemo/WatchDemo.vue
@@ -2,13 +2,20 @@
  * @Date        : 2022-11-04 09:41:26
  * @Author      : ZhouQiJun
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-08-18 00:03:23
+ * @LastEditTime: 2024-01-26 11:57:39
  * @Description : watch 有哪些用法 
 -->
 <template>
   <div>
     <h1>watch</h1>
-    <WatchCleanup />
+    <WatchProps
+      v-model="modelValue"
+      :bool-val="isOk"
+      :number-val="money"
+      :obj="obj"
+      :arr="testArr" />
+    <button @click="changeObjAndModelValue">changeObjAndModelValue</button>
+    <WatchCleanUp />
     <WatchRefDemo />
     <WatchRef />
     <WatchReactive />
@@ -16,10 +23,29 @@
 </template>
 
 <script setup lang="ts">
-import WatchCleanup from './WatchCleanup.vue'
+import WatchCleanUp from './WatchCleanUp.vue'
+import WatchProps from './WatchProps.vue'
 import WatchReactive from './WatchReactive.vue'
 import WatchRef from './WatchRef.vue'
 import WatchRefDemo from './WatchRefDemo.vue'
+
+const modelValue = ref('')
+const isOk = ref(true)
+const money = ref(100)
+const obj = reactive({
+  name: 'zhouQiJun',
+  age: 18,
+})
+const testArr = ref(['1', '2', '3'])
+
+function changeObjAndModelValue() {
+  modelValue.value = 'modelValue' + Math.random()
+  isOk.value = !isOk.value
+  money.value = 100 * Math.random()
+  obj.name = 'zhouQiJun2'
+  obj.age = 20 * Math.random()
+  testArr.value.push(Math.random() + '')
+}
 </script>
 
 <style lang="scss"></style>
---
+++ vue3/delightful-vue3-vite-project/src/components/WatchDemo/WatchProps.vue
@@ -0,0 +1,109 @@
+<!--
+ * @Author      : ZhouQiJun
+ * @Date        : 2024-01-26 11:04:56
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2024-01-26 12:04:07
+ * @Description : 监听 props
+-->
+<script setup lang="ts">
+// const props = defineProps<{
+//   modelValue?: string
+//   boolVal?: boolean
+//   numberVal?: number
+//   obj: {
+//     name: string
+//     age: number
+//   }
+//   arr: string[]
+// }>()
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  boolVal: {
+    type: Boolean,
+    default: false,
+  },
+  numberVal: {
+    type: Number,
+    default: 0,
+  },
+  obj: {
+    type: Object,
+    default: () => ({
+      name: 'zhouQiJun',
+      age: 18,
+    }),
+  },
+  arr: {
+    type: Array,
+    default: () => ['1', '2', '3'],
+  },
+})
+const emit = defineEmits(['update:modelValue'])
+
+// 监听 string
+// watch(
+//   () => props.modelValue,
+//   val => {
+//     console.log('() => props.modelValue', val)
+//   }
+// )
+// string 报错
+// watch(props.modelValue, val => {
+//   console.log('props.modelValue', val)
+// })
+// watch(
+//   () => props.boolVal,
+//   val => {
+//     console.log('() => props.boolVal', val)
+//   }
+// )
+// watch(
+//   () => props.numberVal,
+//   val => {
+//     console.log('() => props.numberVal', val)
+//   }
+// )
+
+// 监听对象、数组
+watch(props.obj, (val, old) => {
+  console.log('props.obj', val, old)
+})
+// NOTE 监听不到
+watch(
+  () => props.obj,
+  (val, old) => {
+    console.log('()=>props.obj', val, old)
+  },
+  {
+    deep: true,
+  }
+)
+// watch(props.arr, (val, old) => {
+//   console.log('props.arr', val, old)
+// })
+// // NOTE 监听不到
+// watch(
+//   () => props.arr,
+//   (val, old) => {
+//     console.log('()=>props.arr', val, old)
+//   },
+//   {
+//     deep: true,
+//   }
+// )
+</script>
+
+<template>
+  <div>
+    <h2>监听 props</h2>
+    <p>modelValue {{ props.modelValue }}</p>
+    <p>boolVal {{ props.boolVal }}</p>
+    <p>numberVal {{ props.numberVal }}</p>
+    <p>props.obj.name --> {{ props.obj?.name }}</p>
+    <p>props.obj.age --> {{ props.obj?.age }}</p>
+    <p>props.arr --> {{ props.arr.join() }}</p>
+  </div>
+</template>
