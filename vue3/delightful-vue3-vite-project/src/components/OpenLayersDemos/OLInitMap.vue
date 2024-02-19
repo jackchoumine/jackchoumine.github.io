dae1f09607959d075875fafe012066ab5d14a64d
jack <jackzhoumine@gmail.com>
2024-02-19 23:14:02 +0800

    feat: 绘制几何要素

--- vue3/delightful-vue3-vite-project/src/components/OpenLayersDemos/OLInitMap.vue
+++ vue3/delightful-vue3-vite-project/src/components/OpenLayersDemos/OLInitMap.vue
@@ -5,18 +5,31 @@
  * @LastEditTime: 2023-07-17 00:52:11
  * @Description : 初始化 ol 地图
 -->
-<script lang="ts" setup>
+<script setup>
 import { Map, View } from 'ol'
 
 import { Attribution } from 'ol/control'
-import { Tile } from 'ol/layer'
-import { TileJSON, XYZ } from 'ol/source'
+import { Draw, Modify, Snap } from 'ol/interaction.js'
+import { Tile, Vector as VectorLayer } from 'ol/layer'
+import { TileJSON, Vector as VectorSource, XYZ } from 'ol/source'
 
 import { GeoList } from './components'
 
 // console.log(Map)
 // console.log(View)
+const vectorSource = new VectorSource()
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: {
+    'fill-color': 'rgba(255, 255, 255, 0.2)',
+    'stroke-color': '#ffcc33',
+    'stroke-width': 2,
+    'circle-radius': 7,
+    'circle-fill-color': '#ffcc33',
+  },
+})
 
+let map = null
 onMounted(initMap)
 
 function initMap() {
@@ -54,7 +67,7 @@ function initMap() {
     url: tianDiTuUrl5,
   })
 
-  const map = new Map({
+  map = new Map({
     target: 'ol-map-init',
     layers: [
       // new ol.layer.Tile({
@@ -72,6 +85,7 @@ function initMap() {
       new Tile({
         source: tianDiTuSource5,
       }),
+      vectorLayer,
     ],
     // controls: ol.control.defaults.defaults({ attribution: false }).extend([attribution]),
     view: new View({
@@ -82,15 +96,39 @@ function initMap() {
       projection: 'EPSG:4326',
     }),
   })
+  const modify = new Modify({ source: vectorSource })
+  map.addInteraction(modify)
+  // addInteractions()
   // map.addControl(attribution)
   // map.removeLayer(map.getLayers().getArray()[0])
   // map.addLayer(new Tile({ source }))
+}
+
+let draw, snap // global so we can remove them later
+
+function addInteractions(type) {
+  draw = new Draw({
+    source: vectorSource,
+    type,
+  })
+  map.addInteraction(draw)
+  snap = new Snap({ source: vectorSource })
+  map.addInteraction(snap)
+}
+function onSelect(type) {
+  map.removeInteraction(draw)
+  if (type === 'None') return
+  addInteractions(type)
+}
+function onUndo() {
+  console.log(123, 'zqj log')
+  draw.removeLastPoint()
 }
 </script>
 
 <template>
   <div class="init-map" id="ol-map-init">
-    <GeoList />
+    <GeoList @select="onSelect" @undo="onUndo" />
   </div>
 </template>
 
