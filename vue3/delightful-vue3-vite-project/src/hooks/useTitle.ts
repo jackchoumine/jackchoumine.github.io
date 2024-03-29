1531e80968cabbef530b71ae796eb24def9991e2
ZhouQiJun <zhouqijun4job1@163.com>
2024-03-29 16:22:00 +0800

    docs: 添加素材

--- vue3/delightful-vue3-vite-project/src/hooks/useTitle.ts
+++ vue3/delightful-vue3-vite-project/src/hooks/useTitle.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2024-02-19 17:36:55
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2024-03-29 15:30:55
+ * @Description :
+ */
 import type { MaybeRef } from '@vueuse/core'
 
 export function useTitle(newTitle?: MaybeRef<string>) {
@@ -7,4 +14,25 @@ export function useTitle(newTitle?: MaybeRef<string>) {
     document.title = _title
   })
   return title
+}
+
+export function useTitle2(title: MaybeRef<string> = document.title, shouldRestore: true) {
+  const cacheTitle = document.title
+  const _title = ref(title)
+
+  watchEffect(() => {
+    document.title = _title.value
+  })
+
+  if (shouldRestore) {
+    onUnmounted(() => {
+      document.title = cacheTitle
+    })
+  }
+
+  function setTile(title: string) {
+    _title.value = title
+  }
+
+  return setTile
 }
