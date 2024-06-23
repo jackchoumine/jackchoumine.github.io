e4684e4804da2b203fc57537f0d9bd73760af8ce
ZhoQiJun <zhouqijun4job@163.com>
2024-06-24 04:05:39 +0800

    feat: Update documentation for Vue 3 component lifecycle events

--- vue3/delightful-vue3-vite-project/src/components/WatchDemo/WatchRefDemo.vue
+++ vue3/delightful-vue3-vite-project/src/components/WatchDemo/WatchRefDemo.vue
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-08-18 00:02:00
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-10-09 16:10:01
+ * @LastEditTime: 2024-06-24 03:37:47
  * @Description : 
 -->
 <script setup lang="ts">
@@ -39,8 +39,8 @@ function changeA() {
 
 const numList = shallowRef<any[]>([1, 2, 3])
 function changeNumList() {
-  // NOTE shallowRef<Array> 无法改变 ❌ ref<Array> 是可以监听到改变的
   // numList.value.push(10, 20, 30)
+  // NOTE shallowRef<Array> 无法改变 ❌ ref<Array> 是可以监听到改变的
   numList.value = [...numList.value, 10, 20, 30]
 }
 
