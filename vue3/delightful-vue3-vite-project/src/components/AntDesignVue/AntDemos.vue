d72df961c70e358f955003543ebb6d97374a0e0d
ZhoQiJun <zhouqijun4job@163.com>
2024-06-23 18:30:49 +0800

    chore: update package.json and import paths

--- vue3/delightful-vue3-vite-project/src/components/AntDesignVue/AntDemos.vue
+++ vue3/delightful-vue3-vite-project/src/components/AntDesignVue/AntDemos.vue
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-02-12 18:41:36
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-05-06 01:16:50
+ * @LastEditTime: 2024-06-22 18:23:45
  * @Description : 
 -->
 <script setup lang="tsx" name="Demos">
@@ -15,12 +15,24 @@ onMounted(() => {
   console.log('ant-design-vue mounted')
   emit('ant-btn-mounted', 'ant-design-vue')
 })
+function whenMounted(p) {
+  console.log('ant-design-vue mounted 12', p)
+}
+function whenBeforeUnmount(p) {
+  console.log('ant-design-vue beforeUnmount', p)
+}
 </script>
 
 <template>
   <div class="vue-component">
     <h2>ant-design-vue</h2>
-    <AButton type="primary" shape="round">测试 ant-design-vue 可用性</AButton>
+    <AButton
+      type="primary"
+      shape="round"
+      @vue:mounted="whenMounted"
+      @vue:beforeUnmount="whenBeforeUnmount">
+      测试 ant-design-vue 可用性
+    </AButton>
   </div>
 </template>
 
