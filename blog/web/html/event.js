020f9fdd0ed77346e07b8913cb7eb60c86599a36
ZhouQiJun <zhouqijun4job1@163.com>
2024-07-10 19:35:43 +0800

    note: 两种阻止事件冒泡

--- blog/web/html/event.js
+++ blog/web/html/event.js
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-12-12 16:44:26
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-12-12 16:57:53
+ * @LastEditTime: 2024-07-10 19:30:52
  * @Description : 封装事件
  */
 // emit.on('event',function () {
@@ -25,5 +25,45 @@ emit.on('my-click', function (data, event) {
 
 btn.onclick = function () {
   emit.fire('my-click', { a: 1 })
-  emit.fire('myClick')
+  // emit.fire('myClick')
 }
+
+const testBtn = document.getElementById('testBtn')
+const span = document.getElementById('spanBtn')
+const div = document.getElementById('divBtn')
+
+const phaseMap = {
+  1: '捕获',
+  2: '目标',
+  3: '冒泡',
+}
+// testBtn.addEventListener(
+//   'click',
+//   function (e) {
+//     console.log('testBtn', phaseMap[e.eventPhase])
+//     // 阻止冒泡
+//     // e.stopPropagation()
+//     // e.stopImmediatePropagation()
+//   },
+//   true
+// )
+testBtn.addEventListener('click', function (e) {
+  console.log('testBtn', phaseMap[e.eventPhase])
+  // return false
+  // e.stopPropagation()
+  // e.stopImmediatePropagation()
+})
+
+span.addEventListener('click', function (e) {
+  console.log('span', phaseMap[e.eventPhase])
+})
+
+div.addEventListener('click', function (e) {
+  console.log('div', phaseMap[e.eventPhase])
+})
+
+// stopImmediatePropagation vs stopPropagation
+// NOTE 笔记
+// 1. 都能阻止事件向祖先元素冒泡
+// 2. stopImmediatePropagation 能阻目标元素的其他相同事件触发，而 stopPropagation 不能
+// 元素可监听多次监听相同事件
