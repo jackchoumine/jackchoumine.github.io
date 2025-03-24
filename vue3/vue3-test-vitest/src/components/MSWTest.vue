1e89269df916fbd10f49e04a4b0e962fc316ea9c
ZhouQiJun <zhouqijun4job@163.com>
2025-03-24 23:47:25 +0800

    feat: 在浏览器里使用 MSW

--- vue3/vue3-test-vitest/src/components/MSWTest.vue
+++ vue3/vue3-test-vitest/src/components/MSWTest.vue
@@ -1,8 +1,18 @@
+<!--
+ * @Author      : ZhouQiJun
+ * @Date        : 2025-03-24 23:06:05
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2025-03-24 23:47:09
+ * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
+ * @加微信         : MasonChou123，进技术交流群
+-->
 <script setup>
 import { ref } from 'vue'
 const users = ref()
 const loading = ref()
+
 getUsers()
+
 function getUsers() {
   loading.value = true
   fetch('/api/users')
