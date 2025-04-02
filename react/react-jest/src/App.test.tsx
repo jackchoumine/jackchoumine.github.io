88504493a313f8dea5df72c2ce3efc4905b88a2d
ZhouQiJun <zhouqijun4job1@163.com>
2025-04-02 16:50:43 +0800

    jest 断言

--- react/react-jest/src/App.test.tsx
+++ react/react-jest/src/App.test.tsx
@@ -2,12 +2,12 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-04-02 15:26:07
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-04-02 16:23:37
+ * @LastEditTime: 2025-04-02 16:35:00
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
  */
-// ./App.test.tsx
-import { mount } from 'enzyme'
+// ./src/App.test.tsx
+import { render, screen } from '@testing-library/react'
 
 import App from './App'
 
@@ -15,9 +15,9 @@ describe('test', () => {
   it('first unit test', () => {
     //  <React.StrictMode>
     // </React.StrictMode>
-    const app = mount(<App />)
-    expect(app.find('.read-the-docs').text()).toEqual(
-      'Click on the Vite and React logos to learn more'
-    )
+    render(<App />)
+    const text = 'Click on the Vite and React logos to learn more'
+    const p = screen.getByText(text)
+    expect(p).toBeInTheDocument()
   })
 })
