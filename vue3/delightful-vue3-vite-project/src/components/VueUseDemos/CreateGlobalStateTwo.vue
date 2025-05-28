3dc7ed6f7984dff0e31076bf2ce117587b04529d
ZhouQiJun <zhouqijun4job1@163.com>
2025-05-28 10:26:49 +0800

    feat: 记录模块变量和闭包实现全局状态

--- vue3/delightful-vue3-vite-project/src/components/VueUseDemos/CreateGlobalStateTwo.vue
+++ vue3/delightful-vue3-vite-project/src/components/VueUseDemos/CreateGlobalStateTwo.vue
@@ -2,8 +2,8 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-05-15 16:44:23
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-12-13 12:12:52
- * @Description : 
+ * @LastEditTime: 2025-05-28 10:03:57
+ * @Description :
 -->
 <script lang="ts" setup name="CreateGlobalState">
 import { useGlobalCounter, useOpen, useOpen2 } from './store'
@@ -22,15 +22,15 @@ function increment() {
 
 <template>
   <div class="create-global-state">
-    <h4>修改全局状态</h4>
+    <h4>使用闭包共享全局状态</h4>
+    <p>count {{ count }}</p>
     <button @click="decrement" class="btn-min">-</button>
     <button @click="increment" class="btn-plus">+</button>
-    <p>count {{ count }}</p>
-    <h4>使用全局变量保存全局状态</h4>
-    <ElButton @click="() => toggleOpen()" type="primary">切换 open {{ isOpen }}</ElButton>
     <ElButton @click="() => toggleOpen2()" type="primary">
       切换 open2 {{ isOpen2 }}
     </ElButton>
+    <h4>使用全局变量保存全局状态</h4>
+    <ElButton @click="() => toggleOpen()" type="primary">切换 open {{ isOpen }}</ElButton>
   </div>
 </template>
 
