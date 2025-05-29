166f2d34dda1220a69b66e820e64089f5fb126c9
ZhouQiJun <zhouqijun4job1@163.com>
2025-05-29 12:04:15 +0800

    feat: 调整布局

--- vue3/tailwind-learn/src/App.vue
+++ vue3/tailwind-learn/src/App.vue
@@ -1,11 +1,17 @@
+<!--
+ * @Author      : ZhouQiJun
+ * @Date        : 2025-05-29 11:32:34
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2025-05-29 12:03:12
+ * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
+ * @加微信         : MasonChou123，进技术交流群
+-->
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+  <header class="bg-gray-100 min-h-auto">
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <nav>
@@ -21,23 +27,16 @@ import { RouterLink, RouterView } from 'vue-router'
 <style scoped>
 header {
   line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
-  margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+  color: green;
 }
 
 nav a.router-link-exact-active:hover {
@@ -48,36 +47,10 @@ nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
+  font-size: 1.2rem;
 }
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- vue3/tailwind-learn/src/views/AboutView.vue
+++ vue3/tailwind-learn/src/views/AboutView.vue
@@ -1,15 +1,15 @@
+<!--
+ * @Author      : ZhouQiJun
+ * @Date        : 2025-05-29 11:32:34
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2025-05-29 11:56:18
+ * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
+ * @加微信         : MasonChou123，进技术交流群
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
