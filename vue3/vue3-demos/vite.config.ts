5118efbcfd94d21bd52dd20e96bde7932b9f39ea
ZhouQiJun <zhouqijun4job@163.com>
2025-06-07 13:42:47 +0800

    build: 样式开发环境开启 sourcemap

--- vue3/vue3-demos/vite.config.ts
+++ vue3/vue3-demos/vite.config.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-12-03 20:34:48
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-06-07 11:08:36
+ * @LastEditTime: 2025-06-07 13:41:35
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
  */
@@ -13,19 +13,27 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'js/[name]-[hash:8].js',
-        chunkFileNames: 'js/[name]-[hash:8].js',
-        assetFileNames: 'assets/[name]-[hash:8][extname]'
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      devSourcemap: true
+      //preprocessorOptions: {
+      //  scss: {}
+      //}
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'js/[name]-[hash:8].js',
+          chunkFileNames: 'js/[name]-[hash:8].js',
+          assetFileNames: 'assets/[name]-[hash:8][extname]'
+        }
       }
     }
   }
