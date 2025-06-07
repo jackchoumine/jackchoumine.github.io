7bcba9baf3bc2a4ead180c64cfeffe58e8f8d429
ZhouQiJun <zhouqijun4job@163.com>
2025-06-07 12:35:45 +0800

    build: 修改 vite 构建输出

--- vue3/vue3-demos/vite.config.ts
+++ vue3/vue3-demos/vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2023-12-03 20:34:48
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2025-06-07 11:08:36
+ * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
+ * @加微信         : MasonChou123，进技术交流群
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,13 +14,19 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash:8].js',
+        chunkFileNames: 'js/[name]-[hash:8].js',
+        assetFileNames: 'assets/[name]-[hash:8][extname]'
+      }
     }
   }
 })
