5be6f959f04aa87f01e976d9f7469687f540cdfb
ZhouQiJun <zhouqijun4job1@163.com>
2025-07-03 15:29:13 +0800

    wip: 二次封装组件的技巧

---
+++ vue3/vue-skills/src/components/MyInput/MyInput.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ElInput } from 'element-plus'
+import type { InputProps } from 'element-plus'
+import { h } from 'vue'
+
+const props = withDefaults(defineProps<Partial<InputProps>>(), {
+  placeholder: '默认的 placeholder',
+  modelValue: ''
+})
+</script>
+
+<template>
+  <div class="MyInput">
+    <h3>二次封装的组件 placeholder {{ props.placeholder }}</h3>
+    <component
+      :is="h(ElInput, { ...props }, $slots)"
+      :model-value="props.modelValue"
+      @input="$emit('update:model-value', $event)"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+.MyInput {
+  // scss code
+}
+</style>
