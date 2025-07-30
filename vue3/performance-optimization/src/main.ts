bb050f066cb95e7186ce4f6dc36fc1102aace124
ZhouQiJun <zhouqijun4job@163.com>
2025-07-31 01:42:06 +0800

    note: 开发环境全量加载 element-plus，生产环境按需加载

--- vue3/performance-optimization/src/main.ts
+++ vue3/performance-optimization/src/main.ts
@@ -2,12 +2,10 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-07-31 00:15:16
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-07-31 01:16:51
+ * @LastEditTime: 2025-07-31 01:36:54
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
  */
-//import ElementPlus from 'element-plus'
-//import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
@@ -19,6 +17,13 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-//.use(ElementPlus)
+
+if (import.meta.env.DEV) {
+  console.log('开发环境全量加载')
+  import('element-plus/dist/index.css')
+  import('element-plus').then(({ default: ElementPlus }) => {
+    app.use(ElementPlus)
+  })
+}
 
 app.mount('#app')
--- vue3/performance-optimization/vite.config.ts
+++ vue3/performance-optimization/vite.config.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-07-31 00:15:16
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-07-31 01:15:41
+ * @LastEditTime: 2025-07-31 01:31:41
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
  */
@@ -20,81 +20,86 @@ import { analyzer } from 'vite-bundle-analyzer'
 import htmlMinifier from 'vite-plugin-html-minifier'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    htmlMinifier(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    analyzer({
-      analyzerPort: 8081,
-      openAnalyzer: false,
-    }),
-    visualizer({ open: true, template: 'sunburst' }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      output: {
-        // 设置输出入口文件名为 main.js
-        //entryFileNames: 'js/[name].js',
-        //chunkFileNames: 'js/[name]-[hash].js',
-        //assetFileNames: '[ext]/[name]-[hash].[ext]',
-        // 入口文件不加 hash
-        entryFileNames: chunkInfo => {
-          if (chunkInfo.name === 'main') {
-            return 'js/main.js' // 不加 hash
-          }
-          return 'js/[name]-[hash].js'
+export default defineConfig(({ mode, command }) => {
+  console.log({ mode, command })
+  const plugins = [vue(), vueJsx()]
+  if (mode === 'production') {
+    plugins.push(
+      htmlMinifier(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      analyzer({
+        analyzerPort: 8081,
+        openAnalyzer: false,
+      }),
+      visualizer({ open: true, template: 'sunburst' })
+    )
+  }
+  return {
+    plugins,
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
         },
-        // 动态分包的 chunk 根据 name 判断是否加 hash
-        chunkFileNames: chunkInfo => {
-          if (['vue', 'vue-router', 'pinia', 'element-plus'].includes(chunkInfo.name)) {
-            return `js/${chunkInfo.name}.js`
-          }
-          return 'js/[name]-[hash].js'
-        },
-        assetFileNames: chunkInfo => {
-          const chunkName = chunkInfo.name
-          if (chunkName?.includes('element-plus')) {
-            return '[ext]/[name].[ext]'
-          }
-          return '[ext]/[name]-[hash].[ext]'
-        },
+        output: {
+          // 设置输出入口文件名为 main.js
+          //entryFileNames: 'js/[name].js',
+          //chunkFileNames: 'js/[name]-[hash].js',
+          //assetFileNames: '[ext]/[name]-[hash].[ext]',
+          // 入口文件不加 hash
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'main') {
+              return 'js/main.js' // 不加 hash
+            }
+            return 'js/[name]-[hash].js'
+          },
+          // 动态分包的 chunk 根据 name 判断是否加 hash
+          chunkFileNames: chunkInfo => {
+            if (['vue', 'vue-router', 'pinia', 'element-plus'].includes(chunkInfo.name)) {
+              return `js/${chunkInfo.name}.js`
+            }
+            return 'js/[name]-[hash].js'
+          },
+          assetFileNames: chunkInfo => {
+            const chunkName = chunkInfo.name
+            if (chunkName?.includes('element-plus')) {
+              return '[ext]/[name].[ext]'
+            }
+            return '[ext]/[name]-[hash].[ext]'
+          },
 
-        // ✅ 手动分包配置
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // 第三方依赖
-            const index = id.indexOf('node_modules')
-            const end = id.slice(index)
-            const [_0, dep] = end.split('/')
-            //console.log({ dep }, 'zqj')
-            if (dep.includes('vue-router')) return 'vue-router'
-            if (dep.includes('vue')) return 'vue'
-            if (dep.includes('pinia')) return 'pinia'
-            if (dep.startsWith('lodash-es')) return 'lodash-es'
-            //if (dep.startsWith('lodash')) return 'lodash'
-            if (dep.startsWith('element-plus')) return 'element-plus'
-            return 'vendor' // 其他库统一放这里
-          }
+          // ✅ 手动分包配置
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // 第三方依赖
+              const index = id.indexOf('node_modules')
+              const end = id.slice(index)
+              const [_0, dep] = end.split('/')
+              //console.log({ dep }, 'zqj')
+              if (dep.includes('vue-router')) return 'vue-router'
+              if (dep.includes('vue')) return 'vue'
+              if (dep.includes('pinia')) return 'pinia'
+              if (dep.startsWith('lodash-es')) return 'lodash-es'
+              //if (dep.startsWith('lodash')) return 'lodash'
+              if (dep.startsWith('element-plus')) return 'element-plus'
+              return 'vendor' // 其他库统一放这里
+            }
+          },
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    open: true,
-  },
+    server: {
+      open: true,
+    },
+  }
 })
