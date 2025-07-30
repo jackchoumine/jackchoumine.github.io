b36f84b62f4c42cc08197b920298646031a500f6
ZhouQiJun <zhouqijun4job1@163.com>
2025-07-30 16:59:37 +0800

    chore: 指定打包分析端口

--- vue3/performance-optimization/vite.config.ts
+++ vue3/performance-optimization/vite.config.ts
@@ -6,7 +6,13 @@ import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), analyzer()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    analyzer({
+      analyzerPort: 8081,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
