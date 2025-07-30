7fc29086fbfc9b077a6eaf1c4555a2dc1031bf99
ZhouQiJun <zhouqijun4job1@163.com>
2025-07-30 16:44:01 +0800

    chore: 降低依赖，使之和公司项目一致

--- vue3/performance-optimization/vite.config.ts
+++ vue3/performance-optimization/vite.config.ts
@@ -1,20 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    open: true,
   },
 })
