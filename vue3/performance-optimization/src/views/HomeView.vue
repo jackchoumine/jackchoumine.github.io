eb7f19d8826548b3245bcc87fb2ec00b743c10da
ZhouQiJun <zhouqijun4job@163.com>
2025-08-02 02:45:29 +0800

    note: 压缩字体，达到90% 的压缩效果

--- vue3/performance-optimization/src/views/HomeView.vue
+++ vue3/performance-optimization/src/views/HomeView.vue
@@ -2,14 +2,19 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-07-30 16:28:04
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-08-02 00:41:15
+ * @LastEditTime: 2025-08-02 02:42:45
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
 -->
 <script setup lang="ts">
+import { ref, shallowRef } from 'vue'
+
+import { css as fontDingLieSongKeTi } from '@/assets/fonts/DingLieSongKeTi/dingliesongtypeface20241217-2.ttf'
 import imgSrc from '@/assets/test_2025-08-02.png'
 import TheWelcome from '@/components/TheWelcome.vue'
 import { useCounterStore } from '@/stores/counter'
+
+//console.log({ fontDingLieSongKeTi }, 'zqj log')
 
 const counterStore = useCounterStore()
 const APP_URL = import.meta.env.VITE_APP_URL
@@ -17,6 +22,7 @@ console.log(typeof APP_URL)
 const MODE = import.meta.env.MODE
 const isDev = import.meta.env.DEV
 const isProd = import.meta.env.PROD
+const dingLieSongKeTi = shallowRef(fontDingLieSongKeTi)
 </script>
 
 <template>
@@ -27,6 +33,10 @@ const isProd = import.meta.env.PROD
       <h4>{{ APP_URL }}</h4>
       <h4>isDev {{ isDev }} isProd {{ isProd }}</h4>
       <h4>MODE {{ MODE }}</h4>
+      <p class="DingLieSongKeTi" style="font-weight: 900">白日依山尽</p>
+      <p class="DingLieSongKeTi normal">白日依山尽</p>
+      <p class="DingLieSongKeTi border">白日依山尽</p>
+      <p>白日依山尽</p>
       <div>
         <el-button type="primary" @click="counterStore.increment">+</el-button>
       </div>
@@ -35,3 +45,20 @@ const isProd = import.meta.env.PROD
     <TheWelcome />
   </main>
 </template>
+
+<style>
+p {
+  font-size: 40px;
+}
+.normal {
+  font-weight: normal;
+}
+.bolder {
+  font-weight: bolder;
+}
+.DingLieSongKeTi {
+  /*font-family: dingliesongtypeface;*/
+  /*font-weight: v-bind(css.bolder);*/
+  font-family: v-bind(dingLieSongKeTi.family);
+}
+</style>
--- vue3/performance-optimization/vite.config.ts
+++ vue3/performance-optimization/vite.config.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-07-31 00:15:16
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-08-02 01:35:17
+ * @LastEditTime: 2025-08-02 02:34:19
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
  */
@@ -16,6 +16,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import { analyzer } from 'vite-bundle-analyzer'
+import Font from 'vite-plugin-font'
 import VitePluginHtmlEnv from 'vite-plugin-html-env'
 import htmlMinifier from 'vite-plugin-html-minifier'
 import minipic from 'vite-plugin-minipic'
@@ -27,7 +28,14 @@ const picExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp']
 // https://vite.dev/config/
 export default defineConfig(({ mode, command }) => {
   console.log({ mode, command })
-  const plugins = [vue(), vueJsx(), VitePluginHtmlEnv()]
+  const plugins = [
+    vue(),
+    vueJsx(),
+    VitePluginHtmlEnv(),
+    Font.vite({
+      scanFiles: ['src/**/*.{vue,ts,tsx,js,jsx}'],
+    }),
+  ]
   // 使用 command 来区分生产环境和开发环境 command 的值只有两种
   // NOTE 不要使用 MODE 因为的值任意
   if (command === 'build') {
