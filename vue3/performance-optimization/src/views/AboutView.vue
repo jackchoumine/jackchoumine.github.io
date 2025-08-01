28bab0e63fa88d6d68bcddeafd5bc287cf37d1d0
ZhouQiJun <zhouqijun4job@163.com>
2025-08-02 01:37:33 +0800

    note: 压缩 public 目录下的代码

--- vue3/performance-optimization/src/views/AboutView.vue
+++ vue3/performance-optimization/src/views/AboutView.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author      : ZhouQiJun
+ * @Date        : 2025-07-31 00:15:16
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2025-08-02 01:32:48
+ * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
+ * @加微信         : MasonChou123，进技术交流群
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
@@ -5,11 +13,11 @@
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: aqua;
+  background-image: url('/test_in_public.png');
 }
 </style>
--- vue3/performance-optimization/vite.config.ts
+++ vue3/performance-optimization/vite.config.ts
@@ -2,7 +2,7 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-07-31 00:15:16
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-08-02 01:17:11
+ * @LastEditTime: 2025-08-02 01:35:17
  * @Description : 关于博主，前端程序员，最近专注于 webGis 开发
  * @加微信         : MasonChou123，进技术交流群
  */
