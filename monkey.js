748abd9c85d6304415e691ff91d9c2c450b48e3b
ZhouQiJun <zhouqijun4job@163.com>
2025-08-18 04:54:01 +0800

    feat: 压缩脚本

--- monkey.js
+++ monkey.js
@@ -7,6 +7,7 @@
 // @match        https://www.npmjs.com/package/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=npmjs.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 ;(function () {
   const sortStyle = `
